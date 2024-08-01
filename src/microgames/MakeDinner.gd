4ffd0cc34974113f87625445c5ce3b512b449ea8
Russell Matney <russell.matney@gmail.com>
2024-08-01 16:23:23 -0400

    fix: more readable makeDinner layout
    Larger text, some mouse cursor work.

--- src/microgames/MakeDinner.gd
+++ src/microgames/MakeDinner.gd
@@ -8,6 +8,8 @@ var bad_ingredients = []
 
 var held_object = null
 
+var added_poison = false
+
 func _ready():
 	for node in get_tree().get_nodes_in_group("good_ingredients"):
 		good_ingredients.append(node)
@@ -16,13 +18,19 @@ func _ready():
 
 	pot.body_entered.connect(on_body_entered_pot)
 
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+
 	for node in get_tree().get_nodes_in_group("draggable"):
 		node.clicked.connect(_on_pickable_clicked)
+
+func _exit_tree():
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
 
 func _on_pickable_clicked(object):
 	if !held_object:
 		object.pickup()
 		held_object = object
+		Input.set_default_cursor_shape(Input.CURSOR_DRAG)
 
 
 func _unhandled_input(event):
@@ -30,15 +38,18 @@ func _unhandled_input(event):
 		if held_object and !event.pressed:
 			held_object.drop()
 			held_object = null
+			Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
 
 func on_body_entered_pot(body):
 	if body in good_ingredients:
 		good_ingredients.erase(body)
 
-		# added ingredient sound!
+		Sounds.play(Sounds.S.pray)
 		body.set_visible(false)
-		if good_ingredients.is_empty():
+		if not added_poison and good_ingredients.is_empty():
 			mg.game_won.emit()
 	elif body in bad_ingredients:
 		mg.game_lost.emit()
 		body.set_visible(false)
+		added_poison = true
+		Sounds.play(Sounds.S.new_order)
